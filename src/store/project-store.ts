76068b7228d89e05aa2254c71bd8d74bb27ab1e0
우명 <umyeong@umyeong-ui-MacBookPro.local>
2026-04-07 09:11:49 +0900

    estimate first

--- src/store/project-store.ts
+++ src/store/project-store.ts
@@ -2,7 +2,7 @@
 
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { SelectedMaterial, ScopeItem, HousingType } from "@/types";
+import { SelectedMaterial, ScopeItem, HousingType } from "../types";
 
 type ProjectState = {
   address: string;
